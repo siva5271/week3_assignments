dbba4fabb3b604d7690ad1e2a4e9c42ff3225bbb
siva5271 <71comingsoon@gmail.com>
2023-04-23 11:23:30 +0530

    q6 complete

---
+++ q6.py
@@ -0,0 +1,18 @@
+userEntry=int(input("Enter a number between 0 and 8:"))
+match userEntry:
+    case 1:
+        print("Sunday")
+    case 2:
+        print("Monday")
+    case 3:
+        print("Tuesday")
+    case 4:
+        print("Wednesday")
+    case 5:
+        print("Thursday")
+    case 6:
+        print("Friday")
+    case 7:
+        print("Saturday")
+    case _:
+        print("Enter a valid value!")
