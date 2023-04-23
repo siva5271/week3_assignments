ba80948fca315a561f9850a9e172161e030e7dde
siva5271 <71comingsoon@gmail.com>
2023-04-23 11:01:02 +0530

    q3 complete

---
+++ q3.py
@@ -0,0 +1,5 @@
+principal = int(input("Enter the principal amount:"))
+interest = float(input("Enter the interest rate:"))
+noOfYears = float(input("Enter the number of years:"))
+simpleInterest = (principal * interest * noOfYears) / 100
+print("The calculated simple interest is:", simpleInterest)
