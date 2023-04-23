1b050fb6b7e7df7c9023c5e64a667f8840cd6174
siva5271 <71comingsoon@gmail.com>
2023-04-23 11:14:03 +0530

    q5 complete

---
+++ q5.py
@@ -0,0 +1,13 @@
+mark = float(input("Enter your marks"))
+if mark < 50:
+    print("Failed!")
+elif mark < 60:
+    print("E grade awarded!")
+elif mark < 70:
+    print("D grade awarded!")
+elif mark < 80:
+    print("C grade awarded!")
+elif mark < 90:
+    print("B grade awarded!")
+else:
+    print("A grade awarded!")
